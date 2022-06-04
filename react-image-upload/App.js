b8bb34dfbe09abc82afe364bb0edba1416455ee4
Shawn Freeman <magus_zsf@hotmail.com>
2022-06-04 09:19:23 -0400

    Working dynamic image selection from base64 image

--- react-image-upload/App.js
+++ react-image-upload/App.js
@@ -1,20 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions, Image } from 'react-native';
-import amory_A from './img/amory_A.png';
+import { StyleSheet, Text, View, Dimensions, Image, TouchableOpacity } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import RNFS from 'react-native-fs';
+import imgA from './img/A.png';
 
-export default function App() {
-  return (
-    <View style={localstyles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Image source={amory_A} style={localstyles.myPic} resizeMode="contain" />
-      <Image source={{ uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=='}} style={{ flex: 0.2,width:55, height:55, resizeMode: 'contain' }} />
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    displayImage: imgA,
+  };
+
+  handleChoosePhoto = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+      
+      //data:image/png;base64
+      let base64Image = await RNFS.readFile(result.uri, 'base64');
+      let extension = result.uri.split('.').pop();
+      let uri = `data:image/${extension};base64,${base64Image}`;
+      
+      if (!result.cancelled) {
+        this.setState({ displayImage: { uri: uri }  });
+      }
+};
+
+  render(){
+    return (
+      <View style={localStyles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Image source={this.state.displayImage} style={localStyles.myPic} resizeMode="contain" />
+        <TouchableOpacity
+            style = {localStyles.photoButton}
+            onPress={this.handleChoosePhoto} >
+            <Text style = {localStyles.photoButtonText}>Choose Photo</Text>
+        </TouchableOpacity>
+        <Image source={{ uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=='}} style={{ flex: 0.2,width:55, height:55, resizeMode: 'contain' }} />
+      </View>
+    );
+  }
+  
 }
 
-const localstyles = StyleSheet.create({
+const localStyles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -30,5 +61,18 @@ const localstyles = StyleSheet.create({
     //backgroundColor: 'red',
     width: 200,
     height: 200,
+},
+photoButton: {
+  backgroundColor: '#ccccff',
+  borderColor: '#5453A6',
+  borderWidth: 2,
+  height: 40,
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: (Dimensions.get('window').width / 10) * 9,
+  borderRadius: 10,
+},
+photoButtonText:{
+ color: '#5453A6',
 },
 });
